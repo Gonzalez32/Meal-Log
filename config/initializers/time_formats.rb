3da8561a94b22996c6032fa0865ebd403cea3d8e
Gonzalez32 <3232rose@gmail.com>
2020-10-14 13:29:12 -0500

    archives added.

---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,3 @@
+#time info render on page
+
+Time::DATE_FORMATS[:nice_date] = "%B %e, %Y"
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'archives/index'
+  resources :entries
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "entries#index"
+
+
 end
